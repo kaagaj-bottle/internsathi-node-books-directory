d9137e54e0688c585a4e52d2eb3afb84753d60b1
Zishan <zishansiddique80@gmail.com>
2022-12-18 16:02:49 +0545

    config updated for mongodb

--- utilities/config.js
+++ utilities/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = `mongodb+srv://kaagaj-bottle:${process.env.MONGODB_URI_PASSWORD}@cluster0.kohug.mongodb.net/?retryWrites=true&w=majority`;
 
 module.exports = {
   PORT,
