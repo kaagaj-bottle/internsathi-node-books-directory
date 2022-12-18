96d361e75a6212f86e6e08911c39a48a03b00933
Zishan <zishansiddique80@gmail.com>
2022-12-18 16:08:34 +0545

    config changed for environment variable

--- utilities/config.js
+++ utilities/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const PORT = process.env.PORT;
-const MONGODB_URI = `mongodb+srv://kaagaj-bottle:${process.env.MONGODB_URI_PASSWORD}@cluster0.kohug.mongodb.net/?retryWrites=true&w=majority`;
+const MONGODB_URI = `mongodb+srv://kaagaj-bottle:${process.env.MONGODBURIPASSWORD}@cluster0.kohug.mongodb.net/?retryWrites=true&w=majority`;
 
 module.exports = {
   PORT,
