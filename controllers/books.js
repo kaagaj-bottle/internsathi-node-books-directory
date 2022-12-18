8c702d283c9728131039171ab5775ef9baddbae7
Zishan <zishansiddique80@gmail.com>
2022-12-18 13:22:37 +0545

    GET and POST for the book directory configured

--- controllers/books.js
+++ controllers/books.js
@@ -1,9 +1,61 @@
 const booksRouter = require("express").Router();
 const config = require("../utilities/config");
+const logger = require("../utilities/logger");
 const Book = require("../models/book");
 
 booksRouter.get("/", async (request, response) => {
-  response.json("Hello, world");
+  try {
+    const books = await Book.find({});
+    response.json(books);
+  } catch (err) {
+    logger.error(err);
+  }
+});
+
+booksRouter.post("/", async (request, response) => {
+  const {
+    title,
+    author,
+    price,
+    publisher,
+    description,
+    datePublished,
+    genre,
+    url,
+  } = request.body;
+
+  if (
+    !(
+      title &&
+      author &&
+      price &&
+      publisher &&
+      description &&
+      datePublished &&
+      genre &&
+      url
+    )
+  ) {
+    return response.status(401).json({ err: "missing input or inputs" });
+  }
+
+  const newBook = new Book({
+    title,
+    author,
+    price,
+    publisher,
+    description,
+    datePublished,
+    genre,
+    url,
+  });
+
+  try {
+    const savedBook = await newBook.save();
+    response.json(savedBook);
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
 module.exports = booksRouter;
